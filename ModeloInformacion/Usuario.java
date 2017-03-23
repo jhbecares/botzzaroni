6bf9e3c3ada13c4fabe885420c90903a81aecb27
Jenny Hernández <jhdez.becares@gmail.com>
2017-03-23 13:31:51 +0100

    Añadidos un par de comentarios sobre el modelo de información

--- ModeloInformacion/Usuario.java
+++ ModeloInformacion/Usuario.java
@@ -1,9 +1,9 @@
 
 public class Usuario {
 	
-	private String alias;
+	private String username;
 	private String nombre;
+	private String apellidos;
 	private Direccion direccion;
 	private String movil; // String o int, ahi la duda
-	
 }
