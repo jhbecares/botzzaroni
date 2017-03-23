bd0bfb3e30a80389f5f66c2486e8edc0e30b22f7
Jenny Hernández <jhdez.becares@gmail.com>
2017-03-23 13:31:51 +0100

    Añadidos un par de comentarios sobre el modelo de información

--- ModeloInformacion/Pizza.java
+++ ModeloInformacion/Pizza.java
@@ -2,6 +2,13 @@ import java.util.List;
 
 public class Pizza {
 	
+	// TODO se debería considerar sacar la masa y el tamanyo de aquí
+	// y, además, también considerar que pizza fuera una interfaz o algo
+	// así y que luego se implemente pizzaCarta y pizzaPersonalizada, 
+	// porque me he dado cuenta
+	// de que hay que guardar cosas más "tochas" como el usuario y tal.
+	// no sé...
+
 	public enum MasaPizza{
 		normal, fina, queso
 	}
@@ -15,7 +22,9 @@ public class Pizza {
 	private TamanioPizza tamanio;
 	private SalsaPizza salsa;
 	private List<Ingrediente> ingredientes;
-	private boolean personalizada;
+	private boolean personalizada; 
+	private Usuario usuarioCreador; // será un admin general si no la ha creado un usuario
+	private String nombrePizza; // nombre de la pizza
 	
 	
 }
