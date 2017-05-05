bdcf584a29d631c4194cafa8983123a3045c332b
Jenny Hernández <jhdez.becares@gmail.com>
2017-05-05 19:17:51 +0200

    El int wrapper que se me había olvidado

---
+++ BotzzaroniDev/src/icaro/aplicaciones/informacion/gestionPizzeria/IntWrapper.java
@@ -0,0 +1,14 @@
+package icaro.aplicaciones.informacion.gestionPizzeria;
+
+public class IntWrapper {
+	private int myInt;
+
+	public int getMyInt() {
+		return myInt;
+	}
+
+	public void setMyInt(int myInt) {
+		this.myInt = myInt;
+	}
+	
+}
