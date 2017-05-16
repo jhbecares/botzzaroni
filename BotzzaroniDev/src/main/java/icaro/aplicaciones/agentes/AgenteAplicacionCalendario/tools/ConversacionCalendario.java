13f8424a8bd42d331274bb40ab171488f51d0504
Jenny Hernández <jhdez.becares@gmail.com>
2017-05-16 18:04:54 +0200

    Añadido el agente calendario al xml de configuración

---
+++ BotzzaroniDev/src/main/java/icaro/aplicaciones/agentes/AgenteAplicacionCalendario/tools/ConversacionCalendario.java
@@ -0,0 +1,35 @@
+package icaro.aplicaciones.agentes.AgenteAplicacionCalendario.tools;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class ConversacionCalendario {
+	
+	private static HashMap<String,List<String>> conversacion;
+	
+	static{
+		conversacion = new HashMap<String, List<String>>();
+		
+		List<String> peticionCalendario = new ArrayList<String>();
+		peticionCalendario.add("¿Cuándo querrás recibir tu pedido?");
+		
+
+		conversacion.put("peticionCalendario", peticionCalendario);
+	
+		
+	}
+	
+	
+	public static String msg(String tipo){
+
+		String msg = null;
+		if(conversacion.get(tipo) != null && conversacion.get(tipo).size() > 0){
+			int index = (int)(System.currentTimeMillis() % conversacion.get(tipo).size());
+			msg = conversacion.get(tipo).get(index);
+		}
+		return msg;
+	}
+	
+
+}
