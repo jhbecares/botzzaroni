4aa23ae7f9df6fc9b29679d3241be6b74077ca8c
Jenny Hernández <jhdez.becares@gmail.com>
2017-05-18 13:19:43 +0200

    Añadidas reglas agente calendario. Aun sin probar

---
+++ BotzzaroniDev/src/main/java/icaro/aplicaciones/agentes/AgenteAplicacionCalendario/objetivos/ObtenerHoraEntrega.java
@@ -0,0 +1,10 @@
+package icaro.aplicaciones.agentes.AgenteAplicacionCalendario.objetivos;
+
+import icaro.infraestructura.entidadesBasicas.procesadorCognitivo.Objetivo;
+
+public class ObtenerHoraEntrega extends Objetivo{
+	
+	public ObtenerHoraEntrega() {
+		super.setgoalId("ObtenerHoraEntrega");
+	}
+}
